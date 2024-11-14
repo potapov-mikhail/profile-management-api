6338cdf7b0bf0c4b82f83b06cabc35530da5a2e4
Mikhail Potapov <mpotapov.it@gmail.com>
2024-11-14 23:23:19 +0700

    Add example of swagger annotations

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,3 +1,4 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsEmail, IsNotEmpty, IsEnum } from 'class-validator';
 
 export enum UserRole {
@@ -8,13 +9,19 @@ export enum UserRole {
 export class CreateUserDto {
   @IsEmail()
   @IsNotEmpty()
+  @ApiProperty({
+    example: 'user@example.com',
+    description: 'Email пользователя',
+  })
   email: string;
 
   @IsString()
   @IsEnum(UserRole)
+  @ApiProperty({ example: 'USER', description: 'Роль пользователя' })
   role: UserRole;
 
   @IsString()
   @IsNotEmpty()
+  @ApiProperty({ example: 'active', description: 'Статус пользователя' })
   status: string;
 }
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,22 +11,30 @@ import {
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('api/users')
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
   @Get()
+  @ApiOperation({ summary: 'Получить всех пользователей' })
+  @ApiResponse({ status: 200, description: 'Список всех пользователей.' })
   async getAllUsers() {
     return this.userService.getAllUsers();
   }
 
   @Get(':id')
+  @ApiOperation({ summary: 'Получить пользователя по ID' })
+  @ApiResponse({ status: 200, description: 'Информация о пользователе.' })
+  @ApiResponse({ status: 404, description: 'Пользователь не найден.' })
   async getUserById(@Param('id') id: string) {
     return this.userService.getUserById(Number(id));
   }
 
   @Post()
+  @ApiOperation({ summary: 'Создать пользователя' })
+  @ApiResponse({ status: 201, description: 'Пользователь успешно создан.' })
   async createUser(@Body() createUserDto: CreateUserDto) {
     return this.userService.createUser(createUserDto);
   }
